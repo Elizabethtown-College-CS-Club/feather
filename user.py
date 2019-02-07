95a4054a78d0abc6466cb05dd77e36d200f5c113
clbx <clay@clbx.io>
2019-02-06 22:48:35 -0500

    basic user class

---
+++ user.py
@@ -0,0 +1,12 @@
+class user:
+    first = "" #First Name
+    last = "" #Last Name
+    email = "" #Email Address
+
+    def __init__(self,first,last,email):  #Python constructor
+        self.first = first
+        self.last = last
+        self.email = email
+
+
+
