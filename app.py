d60d77e64ef4eb7f4353c57e3e050f8f11a73e79
Clay Buxton <clay@clbx.io>
2019-02-06 19:07:26 -0500

    fixing my mistakes :

--- app.py
+++ app.py
@@ -1,46 +1,34 @@
 from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
-from flask_mysqldb import MySQL
-import os
-
+from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password123'
-app.config['MYSQL_DB'] = 'feather'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-mysql = MySQL(app)
+app.debug = True
+app.secret_key = 'This is a random secret key.'
 
 
 @app.route('/')
-def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/handle_login', methods=['POST'])
+def handle_login():
+    if request.form['username'] == 'test':
+        return 'Test' + request.form['username'] + ' ' + request.form['password']
     else:
-        return "Homepage here"
-
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    password = request.form['password']
-    username = request.form['username']
-
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM USERS WHERE USERNAME = \"" + username + "\";")
-    queryresult = cursor.fetchall()
-    print(queryresult)
+        return 'Wrong'
 
 
+@app.route('/dashboard')
+def dashboard():
+    return 'Dashboard home'
 
 
+if __name__ == '__main__':
+    app.run()
 
-    if queryresult[0].get("password") == password:
-        session['logged_in'] = True
-        return home()
-    else:
-        return 'wrong password'
 
-if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=8080)
+"""
+This app in its current status is very insecure. Further measure should be taken to protect the end user from exploits.
+(CSRF)
+"""
